f3472451d68cf49b6e2f209796a836f594f771bb
SosninRoman <sosninroman92@gmail.com>
2024-10-03 21:35:19 +0500

    Add components for online shop app

---
+++ src/shared/shopping/purchasebutton/PurchaseButton.stories.js
@@ -0,0 +1,36 @@
+import { PurchaseButton } from './PurchaseButton'
+
+const meta = {
+    title: 'Otus/Shopping/PurchaseButton',
+    component: PurchaseButton,
+    tags: ['autodocs'],
+    parameters: {
+        docs: {
+            description: {
+                component: 'Button to add item to the cart'
+            }
+        }
+    },
+    argTypes: {
+        count: {
+            description: "Number of items in the cart",
+            control: 'number'
+        },
+    },
+}
+
+export default meta
+
+export const EmptyCartButton = {
+    name: "Without items",
+    args: {
+        count: 0
+    },
+}
+
+export const ButtonWithPurchase = {
+    name: "With items",
+    args: {
+        count: 2
+    },
+}
