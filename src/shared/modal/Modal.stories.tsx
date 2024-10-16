83fa5a073309390b29971d4552d1a7dad01cb4df
SosninRoman <sosninroman92@gmail.com>
2024-10-16 23:03:25 +0500

    [homework4] Add interactive Modal story to storybook

--- src/shared/modal/Modal.stories.tsx
+++ src/shared/modal/Modal.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Modal } from './Modal';
 import type { Meta, StoryObj } from '@storybook/react';
 
@@ -21,4 +21,35 @@ export const Default: StoryObj<typeof Modal> = {
       </p>,
     ],
   },
+};
+
+const OpenModalByClick = () => {
+  const [visible, setVisible] = useState(false);
+  const [modalText, setModalText] = useState('Hello, world!');
+
+  const openModal = () => {
+    if (!visible) {
+      setVisible(true);
+      setTimeout(() => setVisible(false), 2000);
+    }
+  };
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '5px' }}>
+        <input type="text" value={modalText} onChange={(event) => setModalText(event.target.value)} />
+        <button onClick={openModal} disabled={modalText.length === 0}>
+          Show modal
+        </button>
+      </div>
+      <Modal visible={visible}>
+        <p style={{ textAlign: 'center' }}>{modalText}</p>
+      </Modal>
+    </div>
+  );
+};
+
+export const OpenModal: StoryObj<typeof Modal> = {
+  name: 'Interactive modal example',
+  render: () => <OpenModalByClick />,
 };
